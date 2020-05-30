3d9198206945a9c93572c9377aa2f9bc242f6237
Christian Raoulis <craoulis+justchunks@gmail.com>
2020-05-30 12:49:09 +0200

    Initial commit

---
+++ src/main/java/de/blutmondgilde/gifymainmenu/GifyMainMenu.java
@@ -0,0 +1,24 @@
+package de.blutmondgilde.gifymainmenu;
+
+import de.blutmondgilde.gifymainmenu.utils.Ref;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Ref.MOD_ID)
+public class GifyMainMenu {
+    public GifyMainMenu() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
---
+++ src/main/java/de/blutmondgilde/gifymainmenu/utils/Ref.java
@@ -0,0 +1,9 @@
+package de.blutmondgilde.gifymainmenu.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Ref {
+    public static final String MOD_ID = "gifymainmenu";
+    public static final Logger LOGGER = LogManager.getLogger();
+}
